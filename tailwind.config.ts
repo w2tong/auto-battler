a9427b0afa32276695c2309f51b33832a4edaefc
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-05 17:47:24 -0400

    add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                },
+                'health': '#16a34a',
+                'health-bg': '#450a0a',
+
+                'mana': '#2563eb',
+                'mana-bg': '#374151',
+
+                tier: {
+                    0: '#9d9d9d',
+                    1: '#ffffff',
+                    2: '#1eff00',
+                    3: '#0070dd',
+                    4: '#a335ee',
+                    5: '#ff8000'
+                }
+            },
+        }
+    }
+};
