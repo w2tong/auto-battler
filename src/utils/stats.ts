038a5bdf9e7f617fe6547924a389dd18526b6e70
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-08 21:19:12 -0400

    consolidate statFormattingRules to use single function

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -1,20 +1,24 @@
 import { StatType } from "@wholesome-sisters/auto-battler";
 
+function format(num: number, { prefix = '', suffix = '', multiplier = 1 }: { prefix?: string; suffix?: string; multiplier?: number; } = {}): string {
+    return `${prefix}${Number((num * multiplier).toFixed(1))}${suffix}`;
+}
+
 const statFormattingRules: { [stat in StatType]?: { key: string; format: (num: number) => string; } } = {
-    [StatType.BlockChance]: { key: StatType.BlockChance, format: (num) => `${num}%` },
-    [StatType.CriticalChance]: { key: StatType.CriticalChance, format: (num) => `${num}%` },
-    [StatType.CriticalDamage]: { key: StatType.CriticalDamage, format: (num) => `${num * 100}%` },
-    [StatType.DamagePercent]: { key: "Damage", format: (num) => `${num * 100}%` },
-    [StatType.HealthPercent]: { key: "Health", format: (num) => `${(num * 100)}%` },
-    [StatType.PotionEffectiveness]: { key: StatType.PotionEffectiveness, format: (num) => `${num * 100}%` },
-    [StatType.SpellPowerPercent]: { key: "Spell Power", format: (num) => `${num * 100}%` },
+    [StatType.BlockChance]: { key: StatType.BlockChance, format: num => format(num, { suffix: '%' }) },
+    [StatType.CriticalChance]: { key: StatType.CriticalChance, format: num => format(num, { suffix: '%' }) },
+    [StatType.CriticalDamage]: { key: StatType.CriticalDamage, format: num => format(num, { suffix: '%', multiplier: 100 }) },
+    [StatType.DamagePercent]: { key: "Damage", format: num => format(num, { suffix: '%', multiplier: 100 }) },
+    [StatType.HealthPercent]: { key: "Health", format: num => format(num, { suffix: '%', multiplier: 100 }) },
+    [StatType.PotionEffectiveness]: { key: StatType.PotionEffectiveness, format: num => format(num, { suffix: '%', multiplier: 100 }) },
+    [StatType.SpellPowerPercent]: { key: "Spell Power", format: num => format(num, { suffix: '%', multiplier: 100 }) },
 };
 
 function formatStat(stat: StatType, num: number): { key: string, val: string; } {
     const rule = statFormattingRules[stat];
     if (rule) return { key: rule.key, val: rule.format(num) };
     // Default formatting for unhandled stat types
-    return { key: stat, val: num.toString() };
+    return { key: stat, val: format(num) };
 }
 
 export { formatStat };
