a864bf74cef8c750999c6fc40b79a37fec3eeac9
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-12 15:53:19 -0400

    update StatTooltip prop name

--- src/components/StatTooltip.tsx
+++ src/components/StatTooltip.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from "react";
 import Tooltip from "./Tooltip";
 import { StatDescriptions, StatType } from "@wholesome-sisters/auto-battler";
 
-export default function StatTooltip({ children, type, val, display = true }: { children: ReactNode, type: StatType, val: number, display?: boolean; }) {
+export default function StatTooltip({ children, type, num, display = true }: { children: ReactNode, type: StatType, num: number, display?: boolean; }) {
     const content =
         <div className='w-60'>
             <div className='font-bold'>{type}</div>
-            <div>{StatDescriptions[type](val)}</div>
+            <div>{StatDescriptions[type](num)}</div>
         </div>;
 
     return (
--- src/features/CharacterSheet/components/Stat.tsx
+++ src/features/CharacterSheet/components/Stat.tsx
@@ -6,7 +6,7 @@ export default function Stat({ stat, num }: { stat: StatType, num: number; }) {
     const { key, val } = formatStat(stat, num);
     return (
         <div className='w-full flex justify-between'>
-            <StatTooltip type={stat} val={formatStatDescriptionVal(stat, num)}>
+            <StatTooltip type={stat} num={formatStatDescriptionVal(stat, num)}>
                 <span className='mr-4 text-tooltip-highlight font-medium'>{key}</span>
             </StatTooltip>
             <span>{val}</span>
