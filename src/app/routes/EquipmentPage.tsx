c0d042762ace1d85471a5b78461aeea22e506425
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-15 16:46:22 -0400

    Move prop logic from EquipmentPage to CharacterSheet

--- src/app/routes/EquipmentPage.tsx
+++ src/app/routes/EquipmentPage.tsx
@@ -1,6 +1,5 @@
-import { AttributeType, Character, createEquipmentImport, StatType } from "@wholesome-sisters/auto-battler";
+import { Character, createEquipmentImport, startingAbility } from "@wholesome-sisters/auto-battler";
 import CharacterSheet from "../../features/CharacterSheet/CharacterSheet";
-import { WeaponProps } from "../../features/CharacterSheet/components/Weapon";
 import { useCharacters, useSelected } from "../../hooks/Characters/CharactersContext";
 import EquipmentInventory from "../../features/EquipmentInventory/EquipmentInventory";
 
@@ -23,118 +22,16 @@ export default function EquipmentPage() {
         className: selectedChar.class,
         attributes: selectedChar.attributes,
         statTemplate: {},
-        equipment: createEquipmentImport(selectedChar.equipment)
+        equipment: createEquipmentImport(selectedChar.equipment),
+        ability: startingAbility[selectedChar.class]
     });
-
-    const mainHand = char.equipment.mainHand;
-    const { min, max } = char.calcDamageRange({
-        damageRange: mainHand.damageRange,
-        weaponAttack: true,
-        spellPowerRatio: mainHand.spellPowerRatio
-    });
-
-    const mainHandProps: WeaponProps = {
-        name: mainHand.name,
-        type: mainHand.type,
-        min,
-        max,
-        accuracy: char.stats.getAccuracy(mainHand.attackType),
-        damageType: mainHand.damageType,
-        tier: mainHand.tier
-    };
-
-    const offHand = char.equipment.offHandWeapon;
-    let offHandProps: WeaponProps | undefined;
-    if (offHand) {
-        const { min, max } = char.calcDamageRange({
-            damageRange: offHand.damageRange,
-            weaponAttack: true,
-            spellPowerRatio: offHand.spellPowerRatio
-        });
-        offHandProps = {
-            name: offHand.name,
-            type: offHand.type,
-            min,
-            max,
-            accuracy: char.stats.getAccuracy(offHand.attackType) + char.stats.getStat(StatType.OffHandAccuracy),
-            damageType: offHand.damageType,
-            tier: offHand.tier
-        };
-    }
-
-    const str = char.attributes[AttributeType.Strength];
-    const dex = char.attributes[AttributeType.Dexterity];
-    const per = char.attributes[AttributeType.Perception];
-    const con = char.attributes[AttributeType.Constitution];
-    const int = char.attributes[AttributeType.Intelligence];
-    const wis = char.attributes[AttributeType.Wisdom];
 
     return (
         <>
             <h1>Character Page</h1>
             <div className='flex flex-row space-x-4'>
                 <EquipmentInventory />
-                <CharacterSheet name={char.name} level={char.level} className={selectedChar.class} exp={selectedChar.exp}
-                    attributes={{
-                        [AttributeType.Strength]: { base: str.base, bonus: str.bonus },
-                        [AttributeType.Dexterity]: { base: dex.base, bonus: dex.bonus },
-                        [AttributeType.Perception]: { base: per.base, bonus: per.bonus },
-                        [AttributeType.Constitution]: { base: con.base, bonus: con.bonus },
-                        [AttributeType.Intelligence]: { base: int.base, bonus: int.bonus },
-                        [AttributeType.Wisdom]: { base: wis.base, bonus: wis.bonus },
-                    }}
-                    stats={{
-                        [StatType.MaxHealth]: char.stats.maxHealth,
-                        [StatType.HealthPercent]: char.stats.getStat(StatType.HealthPercent),
-                        [StatType.Armour]: char.stats.getStat(StatType.Armour),
-                        [StatType.Deflection]: char.stats.getStat(StatType.Deflection),
-                        [StatType.Dodge]: char.stats.dodge,
-                        [StatType.StatusResistance]: char.stats.getStat(StatType.StatusResistance),
-                        [StatType.Thorns]: char.stats.getStat(StatType.Thorns),
-                        // Block
-                        [StatType.BlockChance]: char.stats.getStat(StatType.BlockChance),
-                        [StatType.BlockPower]: char.stats.getStat(StatType.BlockPower),
-
-                        // Accuracy
-                        [StatType.Accuracy]: char.stats.getStat(StatType.Accuracy),
-                        [StatType.OffHandAccuracy]: char.stats.getStat(StatType.OffHandAccuracy),
-                        [StatType.MeleeAccuracy]: char.stats.getStat(StatType.MeleeAccuracy),
-                        [StatType.RangedAccuracy]: char.stats.getStat(StatType.RangedAccuracy),
-
-                        // Damage
-                        [StatType.Damage]: char.stats.getStat(StatType.Damage),
-                        [StatType.DamagePercent]: char.stats.getStat(StatType.DamagePercent),
-
-                        // Critical
-                        [StatType.CriticalChance]: char.stats.getStat(StatType.CriticalChance),
-                        [StatType.CriticalDamage]: char.stats.getStat(StatType.CriticalDamage),
-
-                        // Defense Reduction
-                        [StatType.ArmourPenetration]: char.stats.getStat(StatType.ArmourPenetration),
-                        [StatType.DodgeReduction]: char.stats.getStat(StatType.DodgeReduction),
-
-                        // Spell
-                        [StatType.SpellAccuracy]: char.stats.getStat(StatType.SpellAccuracy),
-                        [StatType.SpellPower]: char.stats.spellPower,
-                        [StatType.SpellPowerPercent]: char.stats.getStat(StatType.SpellPowerPercent),
-
-                        // Mana
-                        [StatType.ManaCost]: char.stats.getStat(StatType.ManaCost),
-                        [StatType.StartingMana]: char.stats.getStat(StatType.StartingMana),
-                        [StatType.ManaRegen]: char.stats.getStat(StatType.ManaRegen),
-                        [StatType.ManaOnHit]: char.stats.getStat(StatType.ManaOnHit),
-
-                        // Initiative
-                        [StatType.Initiative]: char.stats.getStat(StatType.Initiative),
-
-                        // Potion
-                        [StatType.PotionCharges]: char.stats.getStat(StatType.PotionCharges),
-                        [StatType.PotionHealing]: char.stats.getStat(StatType.PotionHealing),
-                        [StatType.PotionEffectiveness]: char.stats.getStat(StatType.PotionEffectiveness),
-                    }}
-                    mainHand={mainHandProps}
-                    offHand={offHandProps}
-                />
+                <CharacterSheet char={char} exp={selectedChar.exp} />
             </div>
 
         </>
--- src/features/CharacterSheet/CharacterSheet.tsx
+++ src/features/CharacterSheet/CharacterSheet.tsx
@@ -1,37 +1,114 @@
-// import { CharacterInfo, EquipmentItemIds, createEquipment, levelExp, newPlayerChar } from '@wholesome-sisters/auto-battler';
-// import { Character } from '../character-select/character-select-option';
-// import { useEffect, useState } from 'react';
-
-import { AttributeType, ClassName, levelExp, LevelRange, StatType } from "@wholesome-sisters/auto-battler";
+import { AttributeType, Character, levelExp, LevelRange, StatType } from "@wholesome-sisters/auto-battler";
 import Weapon, { WeaponProps } from "./components/Weapon";
 import CharacterSheetAttributes from "./components/CharacterSheetAttributes";
 import CharacterSheetStats from "./components/CharacterSheetStats";
+import CharacterSheetAbility from "./components/CharacterSheetAbility";
 
-// type CharacterSheetProps = {
-//     character: Character;
-//     equipmentItemIds: EquipmentItemIds;
-// }
-
-// export default function CharacterSheet(props: CharacterSheetProps) {
 export default function CharacterSheet(
-    { name, level, className, exp, attributes, stats, mainHand, offHand }:
-        { name: string, level: number, className: ClassName, exp: number, attributes: { [attr in AttributeType]: { base: number, bonus: number; } }, stats: { [stat in StatType]: number }, mainHand: WeaponProps, offHand?: WeaponProps; }
+    { char, exp }: { char: Character, exp: number; }
 ) {
-    // const { character, equipmentItemIds } = props;
-    // const info: CharacterInfo = newPlayerChar('', { name: character.name, level: character.level, class: character.class }, createEquipment(equipmentItemIds)).info();
+
+    // Attributes
+    const str = char.attributes[AttributeType.Strength];
+    const dex = char.attributes[AttributeType.Dexterity];
+    const per = char.attributes[AttributeType.Perception];
+    const con = char.attributes[AttributeType.Constitution];
+    const int = char.attributes[AttributeType.Intelligence];
+    const wis = char.attributes[AttributeType.Wisdom];
+
+    // Weapons
+    const mainHand = char.equipment.mainHand;
+    const mhDamageRange = char.calcDamageRange({ damageRange: mainHand.damageRange, weaponAttack: true, spellPowerRatio: mainHand.spellPowerRatio });
+    const mhProps: WeaponProps = {
+        name: mainHand.name,
+        type: mainHand.type,
+        min: mhDamageRange.min,
+        max: mhDamageRange.max,
+        accuracy: char.stats.getAccuracy(mainHand.attackType),
+        tier: mainHand.tier
+    };
+
+    const offHand = char.equipment.offHandWeapon;
+    let ohProps: WeaponProps | null = null;
+    if (offHand) {
+        const ohDamageRange = char.calcDamageRange({ damageRange: mainHand.damageRange, weaponAttack: true, spellPowerRatio: mainHand.spellPowerRatio });
+        ohProps = {
+            name: offHand.name,
+            type: offHand.type,
+            min: ohDamageRange.min,
+            max: ohDamageRange.max,
+            accuracy: char.stats.getAccuracy(offHand.attackType) + char.stats.getStat(StatType.OffHandAccuracy),
+            tier: offHand.tier
+        };
+    }
 
     return (
         <div>
             <h2>Character Sheet</h2>
-            <h3>{name}</h3>
-            <div>Level {level} {className}</div>
-            <div>{exp}/{levelExp[level as LevelRange]} Experience</div>
+            <h3>{char.name}</h3>
+            <div>Level {char.level} {char.className}</div>
+            <div>{exp}/{levelExp[char.level as LevelRange]} Experience</div>
 
             <br />
 
             <div className='flex flex-row space-x-10'>
-                <CharacterSheetAttributes level={level as LevelRange} attributes={attributes} />
-                <CharacterSheetStats stats={stats} />
+                <CharacterSheetAttributes level={char.level as LevelRange} attributes={{
+                    [AttributeType.Strength]: { base: str.base, bonus: str.bonus },
+                    [AttributeType.Dexterity]: { base: dex.base, bonus: dex.bonus },
+                    [AttributeType.Perception]: { base: per.base, bonus: per.bonus },
+                    [AttributeType.Constitution]: { base: con.base, bonus: con.bonus },
+                    [AttributeType.Intelligence]: { base: int.base, bonus: int.bonus },
+                    [AttributeType.Wisdom]: { base: wis.base, bonus: wis.bonus },
+                }} />
+                <CharacterSheetStats stats={{
+                    [StatType.MaxHealth]: char.stats.maxHealth,
+                    [StatType.HealthPercent]: char.stats.getStat(StatType.HealthPercent),
+                    [StatType.Armour]: char.stats.getStat(StatType.Armour),
+                    [StatType.Deflection]: char.stats.getStat(StatType.Deflection),
+                    [StatType.Dodge]: char.stats.dodge,
+                    [StatType.StatusResistance]: char.stats.getStat(StatType.StatusResistance),
+                    [StatType.Thorns]: char.stats.getStat(StatType.Thorns),
+                    // Block
+                    [StatType.BlockChance]: char.stats.getStat(StatType.BlockChance),
+                    [StatType.BlockPower]: char.stats.getStat(StatType.BlockPower),
+
+                    // Accuracy
+                    [StatType.Accuracy]: char.stats.getStat(StatType.Accuracy),
+                    [StatType.OffHandAccuracy]: char.stats.getStat(StatType.OffHandAccuracy),
+                    [StatType.MeleeAccuracy]: char.stats.getStat(StatType.MeleeAccuracy),
+                    [StatType.RangedAccuracy]: char.stats.getStat(StatType.RangedAccuracy),
+
+                    // Damage
+                    [StatType.Damage]: char.stats.getStat(StatType.Damage),
+                    [StatType.DamagePercent]: char.stats.getStat(StatType.DamagePercent),
+
+                    // Critical
+                    [StatType.CriticalChance]: char.stats.getStat(StatType.CriticalChance),
+                    [StatType.CriticalDamage]: char.stats.getStat(StatType.CriticalDamage),
+
+                    // Defense Reduction
+                    [StatType.ArmourPenetration]: char.stats.getStat(StatType.ArmourPenetration),
+                    [StatType.DodgeReduction]: char.stats.getStat(StatType.DodgeReduction),
+
+                    // Spell
+                    [StatType.SpellAccuracy]: char.stats.getStat(StatType.SpellAccuracy),
+                    [StatType.SpellPower]: char.stats.spellPower,
+                    [StatType.SpellPowerPercent]: char.stats.getStat(StatType.SpellPowerPercent),
+
+                    // Mana
+                    [StatType.ManaCost]: char.stats.getStat(StatType.ManaCost),
+                    [StatType.StartingMana]: char.stats.getStat(StatType.StartingMana),
+                    [StatType.ManaRegen]: char.stats.getStat(StatType.ManaRegen),
+                    [StatType.ManaOnHit]: char.stats.getStat(StatType.ManaOnHit),
+
+                    // Initiative
+                    [StatType.Initiative]: char.stats.getStat(StatType.Initiative),
+
+                    // Potion
+                    [StatType.PotionCharges]: char.stats.getStat(StatType.PotionCharges),
+                    [StatType.PotionHealing]: char.stats.getStat(StatType.PotionHealing),
+                    [StatType.PotionEffectiveness]: char.stats.getStat(StatType.PotionEffectiveness),
+                }} />
             </div>
 
             <br />
@@ -41,18 +118,32 @@ export default function CharacterSheet(
                 <div className='flex flex-row space-x-2'>
                     <div>
                         <h3>Main-hand</h3>
-                        <Weapon name={mainHand.name} type={mainHand.type} min={mainHand.min} max={mainHand.max} accuracy={mainHand.accuracy} damageType={mainHand.damageType} tier={mainHand.tier} />
+                        <Weapon
+                            name={mhProps.name}
+                            type={mhProps.type}
+                            min={mhProps.min}
+                            max={mhProps.max}
+                            accuracy={mhProps.accuracy}
+                            tier={mhProps.tier}
+                        />
                     </div>
-                    {offHand &&
+                    {ohProps &&
                         <div>
                             <h3>Off-hand</h3>
-                            <Weapon name={offHand.name} type={offHand.type} min={offHand.min} max={offHand.max} accuracy={offHand.accuracy} damageType={offHand.damageType} tier={offHand.tier} />
+                            <Weapon
+                                name={ohProps.name}
+                                type={ohProps.type}
+                                min={ohProps.min}
+                                max={ohProps.max}
+                                accuracy={ohProps.accuracy}
+                                tier={ohProps.tier}
+                            />
                         </div>
                     }
                 </div>
             </div>
 
-            <div>PH - ABILITY GOES HERE</div>
+            {char.ability && <CharacterSheetAbility name={char.ability.name} description={char.ability.description(char)} />}
 
             {
                 <div>PH - POTION GOES HERE</div>
