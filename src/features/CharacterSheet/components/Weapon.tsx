26e47867860db4732916773b2f610fb6f8d72eb0
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-15 16:46:40 -0400

    remove DamageType from Weapon

--- src/features/CharacterSheet/components/Weapon.tsx
+++ src/features/CharacterSheet/components/Weapon.tsx
@@ -1,15 +1,15 @@
-import { DamageType, Tier, WeaponType } from "@wholesome-sisters/auto-battler";
+import { Tier, WeaponType } from "@wholesome-sisters/auto-battler";
 import { tierTextColor } from "../../../utils/tierColor";
 
-type WeaponProps = { name: string, type: WeaponType, min: number, max: number, accuracy: number, damageType: DamageType, tier: Tier; };
-function Weapon({ name, type, min, max, accuracy, damageType, tier }: WeaponProps) {
+type WeaponProps = { name: string, type: WeaponType, min: number, max: number, accuracy: number, tier: Tier; };
+function Weapon({ name, type, min, max, accuracy, tier }: WeaponProps) {
     return (
         <div>
             <div className={`font-bold ${tierTextColor[tier]}`}>{name}</div>
             <hr />
             <div>{type}</div>
             <div>{Number(accuracy.toFixed(1))} Accuracy</div>
-            <div>{Number(min.toFixed(1))} - {Number(max.toFixed(1))} {damageType} Damage</div>
+            <div>{Number(min.toFixed(1))} - {Number(max.toFixed(1))} Damage</div>
         </div>
     );
 }
