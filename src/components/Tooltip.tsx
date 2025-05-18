4f2237f77aefda2c84d15ef0ab4737d11678a80c
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-18 17:13:05 -0400

    change Tooltip to inline-block

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Tooltip({ children, content, display = true }: { children: ReactNode, content: ReactNode, display?: boolean; }) {
 
     return (
-        <div className={`group ${display ? 'relative' : ''}`}>
+        <div className={`group inline-block ${display ? 'relative' : ''}`}>
             {children}
             <div className={`absolute bottom-full bg-black text-white px-2 py-1 rounded z-10 border hidden ${display ? 'group-hover:block' : ''}`}>
                 {content}
