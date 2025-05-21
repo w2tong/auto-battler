2290a3295a62b6b4a622bd9320242082a2aed3c7
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-21 18:00:57 -0400

    refactor Item to use isDragging instead of useDndMonitor with state

--- src/features/EquipmentInventory/components/Item.tsx
+++ src/features/EquipmentInventory/components/Item.tsx
@@ -1,18 +1,13 @@
 import { equips } from '@wholesome-sisters/auto-battler';
-import { useDraggable, useDndMonitor } from '@dnd-kit/core';
-import { CSSProperties, useState } from 'react';
+import { useDraggable } from '@dnd-kit/core';
+import { CSSProperties } from 'react';
 import { tierBorderColor } from '../../../utils/tierColor';
 import ItemTooltip from '../../../components/ItemTooltip';
 
 export default function Item({ id, itemId, filtered }: { id: string, itemId: string, filtered: boolean; }) {
-    const [dragging, setDragging] = useState<boolean>(false);
-    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
         id,
-        data: { itemId }
-    });
-    useDndMonitor({
-        onDragStart() { setDragging(true); },
-        onDragEnd() { setDragging(false); }
+        data: { itemId },
     });
     const style: CSSProperties = {
         ...(transform && { transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` })
@@ -22,7 +17,7 @@ export default function Item({ id, itemId, filtered }: { id: string, itemId: str
     // TODO: add tooltip (tooltip loads on hoverover and stays in DOM)
 
     return (
-        <ItemTooltip item={item} display={!dragging}>
+        <ItemTooltip item={item} display={!isDragging}>
             <div style={style} className={`z-10 w-[64px] h-[64px] box-border border-solid rounded-xs bg-black ${tierBorderColor[item.tier]} ${filtered ? '' : 'border-2'}`} ref={setNodeRef} {...listeners} {...attributes}>
                 <img src={`/item-icons/${item.img ? item.img : 'placeholder.png'}`} className={filtered ? 'opacity-20' : 'opacity-100'} />
             </div>
