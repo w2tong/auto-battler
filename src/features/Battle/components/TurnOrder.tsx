50eb5a84f626eb1008721a44d7b799b80d38094a
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-22 16:11:22 -0400

    style TurnOrder characters

--- src/features/Battle/components/TurnOrder.tsx
+++ src/features/Battle/components/TurnOrder.tsx
@@ -5,7 +5,7 @@ export default function TurnOrder({ chars, index }: { chars: string[], index: nu
             <div className='flex flex-row space-x-1'>
                 {chars.map((char, i) => {
                     return (
-                        <div className={i === index ? 'bg-slate-400' : 'bg-slate-900'} key={i}>
+                        <div className={`border rounded-xl px-2 py-0.5 ${i === index ? 'border-slate-600 bg-slate-600' : 'border-slate-900 bg-slate-900'}`} key={i}>
                             {char}
                         </div>
                     );
