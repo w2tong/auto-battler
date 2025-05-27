69fb4f4c64c20f4035eb2a8695d07baff758d1bf
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-26 21:35:22 -0400

    Fix Stat key wrapping to newline instead of single line

--- src/features/CharacterSheet/components/Stat.tsx
+++ src/features/CharacterSheet/components/Stat.tsx
@@ -7,7 +7,7 @@ export default function Stat({ stat, num }: { stat: StatType, num: number; }) {
     return (
         <div className='w-full flex justify-between'>
             <StatTooltip type={stat} num={formatStatDescriptionVal(stat, num)}>
-                <span className='mr-4 text-tooltip-highlight font-medium'>{key}</span>
+                <span className='mr-4 text-tooltip-highlight font-medium whitespace-nowrap'>{key}</span>
             </StatTooltip>
             <span>{val}</span>
         </div>
