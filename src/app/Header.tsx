553fdb436e69def48d7d2bc36f9e7952e417e3b8
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-27 15:22:33 -0400

    Adjust header link colors

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
             <nav className={'flex grow'}>
                 {navLinks.map(({ to, text }, i) =>
                     // <NavLink key={i} to={to} className={({ isActive }) => `${isActive ? 'text-primary-active' : ''} flex-1 h-full hover:text-primary-hover`}>
-                    <NavLink key={i} to={to} className={({ isActive }) => cn('flex-1 h-full hover:text-primary-hover', isActive && 'text-primary-active')}>
+                    <NavLink key={i} to={to} className={({ isActive }) => cn('flex-1 h-full text-primary-text hover:text-primary-text-hover', isActive && 'text-primary-text-active')}>
                         <button className='w-full h-full cursor-pointer text-4xl font-extrabold flex items-center justify-center'>
                             {text}
                         </button>
