66a04aac96e6e526874e82fe4b957d3d0b07f94e
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-02 18:15:56 -0400

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,14 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@contexts": path.resolve(__dirname, "./src/contexts"),
+            "@features": path.resolve(__dirname, "./src/features"),
+            "@util": path.resolve(__dirname, "./src/util"),
+        },
+    },
 });
