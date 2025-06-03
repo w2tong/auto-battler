f177b7e3ad8ebb9222fdd07421b02ad9bdbc65a9
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-03 02:32:24 -0400

    Delete Modal component and remove delete character button from EquipmentPage

--- src/app/routes/EquipmentPage.tsx
+++ src/app/routes/EquipmentPage.tsx
@@ -1,17 +1,11 @@
 import { Character, createEquipmentImport, startingAbility } from "@wholesome-sisters/auto-battler";
 import CharacterSheet from "@features/CharacterSheet/CharacterSheet";
-import { useCharacters, useCharactersDispatch } from "@contexts/Characters/CharactersContext";
+import { useCharacters } from "@contexts/Characters/CharactersContext";
 import EquipmentInventory from "@features/EquipmentInventory/EquipmentInventory";
-import Button from "@components/Button";
-import Modal from "@components/Modal";
-import { useState } from "react";
 
 export default function EquipmentPage() {
     const { list, selected } = useCharacters();
-    const charactersDispatch = useCharactersDispatch();
     const selectedChar = list[selected];
-
-    const [modalOpen, setModalOpen] = useState(false);
 
     if (!selectedChar) {
         return (
@@ -33,22 +27,8 @@ export default function EquipmentPage() {
 
     return (
         <>
-            <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-                <p>Delete {selectedChar.name}?</p>
-                <p>All items currently equipped on {selectedChar.name} will also be deleted.</p>
-                <Button onClick={() => {
-                    charactersDispatch({ type: 'delete', index: selected });
-                    setModalOpen(false);
-                }}>
-                    Yes
-                </Button>
-                <Button onClick={() => setModalOpen(false)}>
-                    No
-                </Button>
-            </Modal>
             <div className='flex flex-row'>
                 <h1>Character Page</h1>
-                <Button onClick={() => setModalOpen(true)}>Delete Character</Button>
             </div>
             <div className='flex flex-row space-x-4'>
                 <EquipmentInventory />
