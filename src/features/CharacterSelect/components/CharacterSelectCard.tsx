9aac7bcc3bd188c01fb62057fdd728a71e367234
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-17 16:51:50 -0400

    remove exp from CharacterSelectCard

--- src/features/CharacterSelect/components/CharacterSelectCard.tsx
+++ src/features/CharacterSelect/components/CharacterSelectCard.tsx
@@ -1,4 +1,4 @@
-import { ClassName, levelExp, LevelRange } from "@wholesome-sisters/auto-battler";
+import { ClassName } from "@wholesome-sisters/auto-battler";
 import ClassIcon from "@components/ClassIcon";
 import { Dispatch, SetStateAction } from "react";
 import { cn } from "@utils/utils";
@@ -13,17 +13,16 @@ type CharacterSelectCardProps = {
     selected: boolean,
     setSelected: Dispatch<SetStateAction<number>>;
 };
-export default function CharacterSelectCard({ name, charClass, level, exp, index, selected, setSelected }: CharacterSelectCardProps) {
+export default function CharacterSelectCard({ name, charClass, level, index, selected, setSelected }: CharacterSelectCardProps) {
     const classColor = classTextColor[charClass];
 
     return (
         <button className={cn('hover:bg-card w-full', selected && 'bg-card')} onClick={() => setSelected(index)}>
             <div className='flex flex-row items-center p-2'>
-                <ClassIcon className='w-[80px] h-[80px]' charClass={charClass} />
+                <ClassIcon className='w-[48px] h-[48px]' charClass={charClass} />
                 <div className='flex flex-col text-left my-auto ml-4 min-w-0'>
                     <div className={cn('font-bold text-lg truncate', classColor)}>{name}</div>
                     <div className='truncate'>Lvl. {level} <span className={cn('text-bold', classColor)}>{charClass}</span></div>
-                    <div className='truncate'>{exp}/{levelExp[level as LevelRange]} exp</div>
                 </div>
             </div>
         </button>
