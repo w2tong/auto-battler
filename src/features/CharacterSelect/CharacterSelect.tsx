8697a8782dae26d7bddb1e8545fccf19b106fc78
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-17 22:06:59 -0400

    close CharacterSelect when clicking on a character card

--- src/features/CharacterSelect/CharacterSelect.tsx
+++ src/features/CharacterSelect/CharacterSelect.tsx
@@ -8,16 +8,14 @@ export default function CharacterSelect() {
     const { list, selected } = useCharacters();
     const characterDispatch = useCharactersDispatch();
     const selectedChar = list[selected];
-    const [open, toggleOpen] = useToggle(false);
-
-    function handleClick() { toggleOpen(); }
+    const [open, toggleOpen, setOpen] = useToggle(false);
 
     const matches = useMediaQuery('(min-width: 640px)');
 
     return <>
         <div
             className={cn('relative flex flex-col z-10 cursor-pointer', matches ? 'bg-sidebar' : 'bg-secondary')}>
-            <div onClick={handleClick}>
+            <div onClick={() => toggleOpen()}>
                 {matches ?
                     <div className='w-60'>
                         {selectedChar ? <CharacterSelectCard name={selectedChar.name} charClass={selectedChar.class} level={selectedChar.level} exp={selectedChar.exp} index={-1} selected={false} setSelected={() => { }} key={-1} /> : 'Select a character'}
@@ -29,9 +27,23 @@ export default function CharacterSelect() {
 
             <div>
                 <div className={cn('absolute bg-secondary right-0', open ? 'block' : 'hidden', matches ? 'w-60' : 'w-screen')}>
-                    {list.map((char, i) => <CharacterSelectCard name={char.name} charClass={char.class} level={char.level} exp={char.exp} index={i} selected={i === selected} setSelected={() => characterDispatch({ type: 'select', index: i })} key={i} />)}
+                    {list.map((char, i) =>
+                        <CharacterSelectCard
+                            key={i}
+                            name={char.name}
+                            charClass={char.class}
+                            level={char.level}
+                            exp={char.exp}
+                            index={i}
+                            selected={i === selected}
+                            setSelected={() => {
+                                characterDispatch({ type: 'select', index: i });
+                                setOpen(false);
+                            }}
+                        />
+                    )}
                 </div>
             </div>
-        </div>
+        </div >
     </>;
 };
