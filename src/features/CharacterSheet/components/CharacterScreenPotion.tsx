0c7f91375991e18604ce5f10a6a517594fdaf0a4
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-19 04:12:31 -0400

    update Potion header to be consistent with Weapon header

--- src/features/CharacterSheet/components/CharacterScreenPotion.tsx
+++ src/features/CharacterSheet/components/CharacterScreenPotion.tsx
@@ -4,7 +4,7 @@ import { tierTextColor } from "@utils/tierColor";
 export default function CharacterSheetPotion({ name, min, max, charges, tier }: { name: string, min: number, max: number, charges: number, tier: Tier; }) {
     return (
         <div>
-            <h4 className='text-lg font-bold'>Potion</h4>
+            <h3 className='text-xl font-bold'>Potion</h3>
             <div className={`font-bold ${tierTextColor[tier]}`}>{name}</div>
             <div><span className='font-bold'>{min} - {max}</span> healing</div>
             <div><span className='font-bold'>{charges}</span> charge{charges > 1 ? 's' : ''}</div>
