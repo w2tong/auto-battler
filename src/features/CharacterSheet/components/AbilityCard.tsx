2c48f938df50f48c23095876ef41d3305b48cd98
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-20 14:45:43 -0400

    increase  Ability name size

--- src/features/CharacterSheet/components/AbilityCard.tsx
+++ src/features/CharacterSheet/components/AbilityCard.tsx
@@ -4,7 +4,7 @@ type AbilityCardProps = { ability: Ability, description: string; };
 export default function AbilityCard({ ability, description }: AbilityCardProps) {
     return (
         <div className='flex flex-col space-y-2 w-full h-full text-left'>
-            <p className='font-bold'>{ability.name}</p>
+            <p className='text-lg font-bold'>{ability.name}</p>
             <p className='text-foreground'>{description}</p>
             <div className='mt-auto'>
                 {ability.attackType && <p className='text-sm'><b>Attack</b>: {ability.attackType}</p>}
