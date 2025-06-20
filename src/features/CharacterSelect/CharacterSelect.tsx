bc20a75ea0c5a6faf941e233af020e7b353d6875
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-19 23:44:14 -0400

    Fix issue where CharacterSelect was not closing after selecting a character

--- src/features/CharacterSelect/CharacterSelect.tsx
+++ src/features/CharacterSelect/CharacterSelect.tsx
@@ -18,48 +18,47 @@ export default function CharacterSelect({ className }: CharacterSelectProps) {
 
     const navigate = useNavigate();
 
-    function handleOpen() {
+    function handleClick() {
         if (list.length > 0) toggleOpen();
-    }
-    function handleUnselected() {
-        if (list.length === 0) navigate('account');
+        else navigate('account');
     }
 
     return (
         <div
             className={cn('relative z-10 cursor-pointer bg-secondary hover:bg-card', className)}
-            onClick={handleOpen}
         >
-            {selectedChar ?
-                <div>
-                    {matches ?
-                        <div className=''>
-                            <CharacterSelectCard name={selectedChar.name} charClass={selectedChar.class} level={selectedChar.level} index={-1} selected={false} setSelected={() => { }} key={-1} />
-                        </div>
-                        :
-                        <div className='p-2'>
-                            <ClassIcon className='w-[48px] h-[48px]' variant='circle' charClass={selectedChar.class} />
-                        </div>
-                    }
-                </div>
-                :
-                <div className='h-full' onClick={handleUnselected}>
-                    {matches ?
-                        <button className={cn('w-full h-full cursor-pointer hover:bg-card')} onClick={handleUnselected}>
-                            <div className='flex flex-row items-center p-2'>
-                                <img className='w-[48px] h-[48px]' src={questionMark} />
-                                <div className='flex flex-col text-left my-auto ml-4 min-w-0 font-bold'>
-                                    Select or Create a character
-                                </div>
+            <div onClick={handleClick}>
+                {selectedChar ?
+                    <>
+                        {matches ?
+                            <div>
+                                <CharacterSelectCard name={selectedChar.name} charClass={selectedChar.class} level={selectedChar.level} index={-1} selected={false} setSelected={() => { }} key={-1} />
                             </div>
-                        </button>
-                        :
-                        <div className='p-2' onClick={() => { if (list.length === 0) toast('Create a character.'); }}>
-                            <img className=' rounded-full w-[48px] h-[48px]' src={questionMark} />
-                        </div>
-                    }
-                </div>
-            }
+                            :
+                            <div className='p-2'>
+                                <ClassIcon className='w-[48px] h-[48px]' variant='circle' charClass={selectedChar.class} />
+                            </div>
+                        }
+                    </>
+                    :
+                    <div className='h-full'>
+                        {matches ?
+                            <button className={cn('w-full h-full cursor-pointer hover:bg-card')}>
+                                <div className='flex flex-row items-center p-2'>
+                                    <img className='w-[48px] h-[48px]' src={questionMark} />
+                                    <div className='flex flex-col text-left my-auto ml-4 min-w-0 font-bold'>
+                                        Select or Create a character
+                                    </div>
+                                </div>
+                            </button>
+                            :
+                            <div className='p-2' onClick={() => { if (list.length === 0) toast('Create a character.'); }}>
+                                <img className=' rounded-full w-[48px] h-[48px]' src={questionMark} />
+                            </div>
+                        }
+                    </div>
+                }
+            </div>
 
             <div>
                 <div className={cn('absolute bg-secondary right-0', open ? 'block' : 'hidden', matches ? 'w-full' : 'w-screen')}>
