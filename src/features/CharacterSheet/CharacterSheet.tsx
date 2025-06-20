9d43169d4d420cba8fd2501c08e4986b93e998fb
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-20 14:18:36 -0400

    Update CharacterSheetStats to take Stats class as prop

--- src/features/CharacterSheet/CharacterSheet.tsx
+++ src/features/CharacterSheet/CharacterSheet.tsx
@@ -86,55 +86,7 @@ export default function CharacterSheet({ char, exp }: CharacterSheetProps) {
 
                 <div className='space-y-2'>
                     <h3 className='text-xl font-bold text-center sm:text-left'>Stats</h3>
-                    <CharacterSheetStats className='flex flex-row flex-wrap flex-1 justify-center sm:justify-start gap-4' stats={{
-                        [StatType.MaxHealth]: char.stats.maxHealth,
-                        [StatType.HealthPercent]: char.stats.getStat(StatType.HealthPercent),
-                        [StatType.Armour]: char.stats.getStat(StatType.Armour),
-                        [StatType.Deflection]: char.stats.getStat(StatType.Deflection),
-                        [StatType.Dodge]: char.stats.dodge,
-                        [StatType.StatusResistance]: char.stats.getStat(StatType.StatusResistance),
-                        [StatType.Thorns]: char.stats.getStat(StatType.Thorns),
-                        // Block
-                        [StatType.BlockChance]: char.stats.getStat(StatType.BlockChance),
-                        [StatType.BlockPower]: char.stats.getStat(StatType.BlockPower),
-
-                        // Accuracy
-                        [StatType.Accuracy]: char.stats.getStat(StatType.Accuracy),
-                        [StatType.OffHandAccuracy]: char.stats.getStat(StatType.OffHandAccuracy),
-                        [StatType.MeleeAccuracy]: char.stats.getStat(StatType.MeleeAccuracy),
-                        [StatType.RangedAccuracy]: char.stats.getStat(StatType.RangedAccuracy),
-
-                        // Damage
-                        [StatType.Damage]: char.stats.getStat(StatType.Damage),
-                        [StatType.DamagePercent]: char.stats.getStat(StatType.DamagePercent),
-
-                        // Critical
-                        [StatType.CriticalChance]: char.stats.getStat(StatType.CriticalChance),
-                        [StatType.CriticalDamage]: char.stats.getStat(StatType.CriticalDamage),
-
-                        // Defense Reduction
-                        [StatType.ArmourPenetration]: char.stats.getStat(StatType.ArmourPenetration),
-                        [StatType.DodgeReduction]: char.stats.getStat(StatType.DodgeReduction),
-
-                        // Spell
-                        [StatType.SpellAccuracy]: char.stats.getStat(StatType.SpellAccuracy),
-                        [StatType.SpellPower]: char.stats.spellPower,
-                        [StatType.SpellPowerPercent]: char.stats.getStat(StatType.SpellPowerPercent),
-
-                        // Mana
-                        [StatType.ManaCost]: char.stats.getStat(StatType.ManaCost),
-                        [StatType.StartingMana]: char.stats.getStat(StatType.StartingMana),
-                        [StatType.ManaRegen]: char.stats.getStat(StatType.ManaRegen),
-                        [StatType.ManaOnHit]: char.stats.getStat(StatType.ManaOnHit),
-
-                        // Initiative
-                        [StatType.Initiative]: char.stats.getStat(StatType.Initiative),
-
-                        // Potion
-                        [StatType.PotionCharges]: char.stats.getStat(StatType.PotionCharges),
-                        [StatType.PotionHealing]: char.stats.getStat(StatType.PotionHealing),
-                        [StatType.PotionEffectiveness]: char.stats.getStat(StatType.PotionEffectiveness),
-                    }} />
+                    <CharacterSheetStats className='flex flex-row flex-wrap flex-1 justify-center sm:justify-start gap-4' stats={char.stats} />
                 </div>
             </div>
 
--- src/features/CharacterSheet/components/CharacterSheetStats.tsx
+++ src/features/CharacterSheet/components/CharacterSheetStats.tsx
@@ -1,4 +1,4 @@
-import { StatType } from "@wholesome-sisters/auto-battler";
+import { Stats, StatType } from "@wholesome-sisters/auto-battler";
 import Stat from "./Stat";
 import { cn } from "@/utils/utils";
 
@@ -6,61 +6,64 @@ const COL_WIDTH = 'w-[184px]';
 const H4_CLASSES = 'text-md font-bold';
 
 // Commented stats are currently have no source from items/abilities/talents
-type CharacterSheetStats = { stats: { [stat in StatType]: number }, className?: string; };
+type CharacterSheetStats = { stats: Stats, className?: string; };
 export default function CharacterSheetStats({ stats, className }: CharacterSheetStats) {
+    const blockChance = stats.getStat(StatType.BlockChance);
+    const blockPower = stats.getStat(StatType.BlockPower);
+
     return (
         <div className={cn('', className)}>
             <div className={COL_WIDTH}>
                 <h4 className={H4_CLASSES}>Defensive</h4>
-                <Stat stat={StatType.MaxHealth} num={stats[StatType.MaxHealth]} />
-                <Stat stat={StatType.Armour} num={stats[StatType.Armour]} />
-                <Stat stat={StatType.Deflection} num={stats[StatType.Deflection]} />
-                <Stat stat={StatType.Dodge} num={stats[StatType.Dodge]} />
-                {/* <Stat stat={StatType.StatusResistance} num={stats[StatType.StatusResistance]} /> */}
-                <Stat stat={StatType.Thorns} num={stats[StatType.Thorns]} />
-                {stats[StatType.BlockChance] !== 0 && stats[StatType.BlockPower] !== 0 &&
+                <Stat stat={StatType.MaxHealth} num={stats.maxHealth} />
+                <Stat stat={StatType.Armour} num={stats.getStat(StatType.Armour)} />
+                <Stat stat={StatType.Deflection} num={stats.getStat(StatType.Deflection)} />
+                <Stat stat={StatType.Dodge} num={stats.dodge} />
+                {/* <Stat stat={StatType.StatusResistance} num={stats.getStat(StatType.StatusResistance)} /> */}
+                <Stat stat={StatType.Thorns} num={stats.getStat(StatType.Thorns)} />
+                {blockChance !== 0 && blockPower !== 0 &&
                     <>
-                        <Stat stat={StatType.BlockChance} num={stats[StatType.BlockChance]} />
-                        <Stat stat={StatType.BlockPower} num={stats[StatType.BlockPower]} />
+                        <Stat stat={StatType.BlockChance} num={blockChance} />
+                        <Stat stat={StatType.BlockPower} num={blockPower} />
                     </>
                 }
             </div>
 
             <div className={COL_WIDTH}>
                 <h4 className={H4_CLASSES}>Offensive</h4>
-                <Stat stat={StatType.Accuracy} num={stats[StatType.Accuracy]} />
-                <Stat stat={StatType.OffHandAccuracy} num={stats[StatType.OffHandAccuracy]} />
-                {/* <Stat stat={StatType.MeleeAccuracy} num={stats[StatType.MeleeAccuracy]} />
-            <Stat stat={StatType.RangedAccuracy} num={stats[StatType.RangedAccuracy]} /> */}
+                <Stat stat={StatType.Accuracy} num={stats.getStat(StatType.Accuracy)} />
+                <Stat stat={StatType.OffHandAccuracy} num={stats.getStat(StatType.OffHandAccuracy)} />
+                {/* <Stat stat={StatType.MeleeAccuracy} num={stats.getStat(StatType.MeleeAccuracy)} />
+            <Stat stat={StatType.RangedAccuracy} num={stats.getStat(StatType.RangedAccuracy} /> */}
 
-                <Stat stat={StatType.Damage} num={stats[StatType.Damage]} />
-                <Stat stat={StatType.DamagePercent} num={stats[StatType.DamagePercent]} />
+                <Stat stat={StatType.Damage} num={stats.damage} />
+                <Stat stat={StatType.DamagePercent} num={stats.getStat(StatType.DamagePercent)} />
 
-                {/* <Stat stat={StatType.SpellAccuracy} num={stats[StatType.SpellAccuracy]} /> */}
-                <Stat stat={StatType.SpellPower} num={stats[StatType.SpellPower]} />
+                {/* <Stat stat={StatType.SpellAccuracy} num={stats.getStat(StatType.SpellAccuracy)} /> */}
+                <Stat stat={StatType.SpellPower} num={stats.spellPower} />
 
-                <Stat stat={StatType.CriticalChance} num={stats[StatType.CriticalChance]} />
-                <Stat stat={StatType.CriticalDamage} num={stats[StatType.CriticalDamage]} />
+                <Stat stat={StatType.CriticalChance} num={stats.critChance} />
+                <Stat stat={StatType.CriticalDamage} num={stats.critDamage} />
 
-                <Stat stat={StatType.ArmourPenetration} num={stats[StatType.ArmourPenetration]} />
-                {/* <Stat stat={StatType.DodgeReduction} num={stats[StatType.DodgeReduction]} /> */}
+                <Stat stat={StatType.ArmourPenetration} num={stats.getStat(StatType.ArmourPenetration)} />
+                {/* <Stat stat={StatType.DodgeReduction} num={stats.getStat(StatType.DodgeReduction)} /> */}
             </div>
 
             <div className={COL_WIDTH}>
                 <h4 className={H4_CLASSES}>Mana</h4>
-                <Stat stat={StatType.ManaCost} num={stats[StatType.ManaCost]} />
-                <Stat stat={StatType.StartingMana} num={stats[StatType.StartingMana]} />
-                <Stat stat={StatType.ManaRegen} num={stats[StatType.ManaRegen]} />
-                <Stat stat={StatType.ManaOnHit} num={stats[StatType.ManaOnHit]} />
+                <Stat stat={StatType.ManaCost} num={stats.getStat(StatType.ManaCost)} />
+                <Stat stat={StatType.StartingMana} num={stats.getStat(StatType.StartingMana)} />
+                <Stat stat={StatType.ManaRegen} num={stats.getStat(StatType.ManaRegen)} />
+                <Stat stat={StatType.ManaOnHit} num={stats.manaOnHit} />
             </div>
 
             <div className={COL_WIDTH}>
                 <h4 className={H4_CLASSES}>Other</h4>
-                <Stat stat={StatType.Initiative} num={stats[StatType.Initiative]} />
+                <Stat stat={StatType.Initiative} num={stats.getStat(StatType.Initiative)} />
 
-                <Stat stat={StatType.PotionCharges} num={stats[StatType.PotionCharges]} />
-                <Stat stat={StatType.PotionHealing} num={stats[StatType.PotionHealing]} />
-                <Stat stat={StatType.PotionEffectiveness} num={stats[StatType.PotionEffectiveness]} />
+                <Stat stat={StatType.PotionCharges} num={stats.getStat(StatType.PotionCharges)} />
+                <Stat stat={StatType.PotionHealing} num={stats.getStat(StatType.PotionHealing)} />
+                <Stat stat={StatType.PotionEffectiveness} num={stats.getStat(StatType.PotionEffectiveness)} />
             </div>
         </div>
     );
